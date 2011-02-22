0901f06be9ab501735fded8375a8974a44d4496c
Han-Wen Nienhuys <hanwen@google.com>
2011-02-22 13:27:43 -0300

    Add test for zipfs.

---
+++ examplelib/zipfs_test.go
@@ -0,0 +1,66 @@
+package examplelib
+
+import (
+	"github.com/hanwen/go-fuse/fuse"
+	"os"
+	"testing"
+	"fmt"
+)
+
+func CheckSuccess(e os.Error) {
+	if e != nil {
+		panic(fmt.Sprintf("Unexpected error: %v", e))
+	}
+}
+
+func TestZipFs(t *testing.T) {
+	wd, err := os.Getwd()
+	CheckSuccess(err)
+	zfs := NewZipFileFuse(wd + "/test.zip")
+	
+	connector := fuse.NewPathFileSystemConnector(zfs)
+	mountPoint := fuse.MakeTempDir()
+	
+	state := fuse.NewMountState(connector)
+	state.Mount(mountPoint)
+
+	go state.Loop(false)
+
+	d, err := os.Open(mountPoint, os.O_RDONLY, 0)
+	CheckSuccess(err)
+	
+	names, err := d.Readdirnames(-1)
+	CheckSuccess(err)
+	err = d.Close()
+	CheckSuccess(err)
+	
+	if len(names) != 2 {
+		t.Error("wrong length", names)
+	}
+	fi, err := os.Stat(mountPoint + "/subdir")
+	CheckSuccess(err)
+	if !fi.IsDirectory() {
+		t.Error("directory type", fi)
+	}
+
+	fi, err = os.Stat(mountPoint + "/file.txt")
+	CheckSuccess(err)
+	
+	if !fi.IsRegular() {
+		t.Error("file type", fi)
+	}
+
+	f, err := os.Open(mountPoint + "/file.txt", os.O_RDONLY, 0)
+	CheckSuccess(err)
+
+	b := make([]byte, 1024)
+	n, err := f.Read(b)
+
+	b = b[:n]
+	if string(b[:n]) != "hello\n" {
+		t.Error("content fail", b[:n])
+	}
+	f.Close()
+	
+	state.Unmount()
+}
