b0878b4b2c1e48db4df4d412068a3baaaefc2436
Han-Wen Nienhuys <hanwen@google.com>
2011-03-23 11:00:01 -0300

    update all.bash

--- all.bash
+++ all.bash
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eux
-for d in fuse examplelib example zipfs
+for d in fuse examplelib example/loopback example/zipfs \
+    example/bulkstat example/multizip 
 do
   gomake -C $d
 done
