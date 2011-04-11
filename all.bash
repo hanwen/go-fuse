bc5d4287efcc94c86323a628be96d94db32ab82a
Han-Wen Nienhuys <hanwen@google.com>
2011-04-11 16:47:32 -0300

    Pass arguments to gomake in all.bash.

--- all.bash
+++ all.bash
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -eux
+
 for d in fuse examplelib example/loopback example/zipfs \
-    example/bulkstat example/multizip 
+    example/bulkstat example/multizip
 do
-  gomake -C $d
+  gomake -C $d "$@"
 done
 
 for d in fuse examplelib
