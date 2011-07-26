bee7c06690964449c3e138b43d67982ea49b87b8
Han-Wen Nienhuys <hanwen@google.com>
2011-07-26 11:22:00 -0300

    all.bash: make clean before rebuilding.

--- all.bash
+++ all.bash
@@ -3,12 +3,14 @@ set -eux
 
 rm -f fuse/version.gen.go
 
-for d in fuse benchmark zipfs unionfs \
+for target in "clean" "" "$@" ; do
+  for d in fuse benchmark zipfs unionfs \
     example/hello example/loopback example/zipfs \
     example/bulkstat example/multizip example/unionfs \
     example/autounionfs ; \
-do
-  gomake -C $d "$@"
+  do
+    gomake -C $d $target
+  done
 done
 
 for d in fuse zipfs unionfs
