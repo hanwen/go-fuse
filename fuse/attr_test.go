7b83201660c6a8d831c0e596cb44b381fdbe045a
Han-Wen Nienhuys <hanwen@google.com>
2011-12-06 02:33:10 -0200

    Add attr_test.go

---
+++ fuse/attr_test.go
@@ -0,0 +1,12 @@
+package fuse
+import (
+	"syscall"
+	"testing"
+)
+
+func TestFileMode(t *testing.T) {
+	sock := FileMode(syscall.S_IFSOCK)
+	if sock.IsDir() {
+		t.Error("Socket should not be directory")
+	}
+}
