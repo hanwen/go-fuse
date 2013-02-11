a0003691a0f8deb64cb52e07234adb7110e334bf
Han-Wen Nienhuys <hanwen@google.com>
2013-02-11 23:31:25 +0100

    Document VerboseTest.

--- fuse/misc.go
+++ fuse/misc.go
@@ -97,6 +97,8 @@ func CurrentOwner() *Owner {
 	}
 }
 
+
+// VerboseTest returns true if the testing framework is run with -v.
 func VerboseTest() bool {
 	flag := flag.Lookup("test.v")
 	return flag != nil && flag.Value.String() == "true"
