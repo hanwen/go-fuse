7383bdf89483a2830303c148ecfbd8558b4fe464
Han-Wen Nienhuys <hanwen@google.com>
2013-02-11 12:06:02 +0100

    Split off splice calls into separate file.

--- splice/pair.go
+++ splice/pair.go
@@ -1,9 +1,7 @@
 package splice
 
 import (
-	"fmt"
 	"os"
-	"syscall"
 )
 
 type Pair struct {
@@ -64,28 +62,4 @@ func (p *Pair) Write(d []byte) (n int, err error) {
 	return p.w.Write(d)
 }
 
-func (p *Pair) LoadFromAt(fd uintptr, sz int, off int64) (int, error) {
-	n, err := syscall.Splice(int(fd), &off, int(p.w.Fd()), nil, sz, 0)
-	return int(n), err
-}
 
-func (p *Pair) LoadFrom(fd uintptr, sz int) (int, error) {
-	if sz > p.size {
-		return 0, fmt.Errorf("LoadFrom: not enough space %d, %d",
-			sz, p.size)
-	}
-
-	n, err := syscall.Splice(int(fd), nil, int(p.w.Fd()), nil, sz, 0)
-	if err != nil {
-		err = os.NewSyscallError("Splice load from", err)
-	}
-	return int(n), err
-}
-
-func (p *Pair) WriteTo(fd uintptr, n int) (int, error) {
-	m, err := syscall.Splice(int(p.r.Fd()), nil, int(fd), nil, int(n), 0)
-	if err != nil {
-		err = os.NewSyscallError("Splice write", err)
-	}
-	return int(m), err
-}
