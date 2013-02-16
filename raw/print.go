4ad779ad937763d2ee724d19213f570b79e86007
Han-Wen Nienhuys <hanwen@google.com>
2013-02-16 14:38:08 +0100

    Fix compile on Linux.

--- raw/print.go
+++ raw/print.go
@@ -210,21 +210,6 @@ func (me *StatfsOut) String() string {
 		me.Bsize, me.NameLen, me.Frsize)
 }
 
-func (a *Attr) String() string {
-	return fmt.Sprintf(
-		"{M0%o SZ=%d L=%d "+
-			"%d:%d "+
-			"%d*%d %d:%d "+
-			"A %d.%09d "+
-			"M %d.%09d "+
-			"C %d.%09d}",
-		a.Mode, a.Size, a.Nlink,
-		a.Uid, a.Gid,
-		a.Blocks, a.Blksize,
-		a.Rdev, a.Ino, a.Atime, a.Atimensec, a.Mtime, a.Mtimensec,
-		a.Ctime, a.Ctimensec)
-}
-
 func (me *ReadIn) String() string {
 	return fmt.Sprintf("{Fh %d off %d sz %d %s L %d %s}",
 		me.Fh, me.Offset, me.Size,
