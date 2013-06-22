63eae3b72feb387fad20d1386ca2861c43fd52d7
Han-Wen Nienhuys <hanwen@google.com>
2013-06-22 14:16:18 +0200

    Add new packages to all.bash.

--- all.bash
+++ all.bash
@@ -4,7 +4,7 @@ set -eux
 sh genversion.sh fuse/version.gen.go
 
 for target in "clean" "install" ; do
-  for d in raw fuse zipfs unionfs \
+  for d in raw fuse fuse/pathfs fuse/test zipfs unionfs \
     example/hello example/loopback example/zipfs \
     example/multizip example/unionfs \
     example/autounionfs ; \
