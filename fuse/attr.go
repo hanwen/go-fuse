b248a51112c52e5230238cde79baaff4c4dbfa74
Han-Wen Nienhuys <hanwen@google.com>
2013-07-07 23:34:04 +0200

    fuse: remove unused type FileMode

--- fuse/attr.go
+++ fuse/attr.go
@@ -1,55 +1,10 @@
 package fuse
 
 import (
-	"log"
 	"os"
 	"syscall"
 	"time"
 )
-
-type FileMode uint32
-
-func (me FileMode) String() string {
-	switch uint32(me) & syscall.S_IFMT {
-	case syscall.S_IFIFO:
-		return "p"
-	case syscall.S_IFCHR:
-		return "c"
-	case syscall.S_IFDIR:
-		return "d"
-	case syscall.S_IFBLK:
-		return "b"
-	case syscall.S_IFREG:
-		return "f"
-	case syscall.S_IFLNK:
-		return "l"
-	case syscall.S_IFSOCK:
-		return "s"
-	default:
-		log.Panic("Unknown mode: %o", me)
-	}
-	return "0"
-}
-
-func (m FileMode) IsFifo() bool { return (uint32(m) & syscall.S_IFMT) == syscall.S_IFIFO }
-
-// IsChar reports whether the FileInfo describes a character special file.
-func (m FileMode) IsChar() bool { return (uint32(m) & syscall.S_IFMT) == syscall.S_IFCHR }
-
-// IsDir reports whether the FileInfo describes a directory.
-func (m FileMode) IsDir() bool { return (uint32(m) & syscall.S_IFMT) == syscall.S_IFDIR }
-
-// IsBlock reports whether the FileInfo describes a block special file.
-func (m FileMode) IsBlock() bool { return (uint32(m) & syscall.S_IFMT) == syscall.S_IFBLK }
-
-// IsRegular reports whether the FileInfo describes a regular file.
-func (m FileMode) IsRegular() bool { return (uint32(m) & syscall.S_IFMT) == syscall.S_IFREG }
-
-// IsSymlink reports whether the FileInfo describes a symbolic link.
-func (m FileMode) IsSymlink() bool { return (uint32(m) & syscall.S_IFMT) == syscall.S_IFLNK }
-
-// IsSocket reports whether the FileInfo describes a socket.
-func (m FileMode) IsSocket() bool { return (uint32(m) & syscall.S_IFMT) == syscall.S_IFSOCK }
 
 func (a *Attr) IsFifo() bool { return (uint32(a.Mode) & syscall.S_IFMT) == syscall.S_IFIFO }
 
