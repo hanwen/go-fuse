1b338d3e6f0c8431387e755e8c4a6bade213c0f9
Han-Wen Nienhuys <hanwen@google.com>
2013-12-30 15:06:32 +0100

    Document WithFlags.String.

--- fuse/nodefs/print.go
+++ fuse/nodefs/print.go
@@ -6,8 +6,9 @@ import (
 	"github.com/hanwen/go-fuse/fuse"
 )
 
-func (me *WithFlags) String() string {
+// String provides a debug string for the given file.
+func (f *WithFlags) String() string {
 	return fmt.Sprintf("File %s (%s) %s %s",
-		me.File, me.Description, fuse.FlagString(fuse.OpenFlagNames, int64(me.OpenFlags), "O_RDONLY"),
-		fuse.FlagString(fuse.FuseOpenFlagNames, int64(me.FuseFlags), ""))
+		f.File, f.Description, fuse.FlagString(fuse.OpenFlagNames, int64(f.OpenFlags), "O_RDONLY"),
+		fuse.FlagString(fuse.FuseOpenFlagNames, int64(f.FuseFlags), ""))
 }
