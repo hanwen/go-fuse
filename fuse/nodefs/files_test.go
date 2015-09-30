c2130ac9e52cdd7ec9b819922fc9a8c5aba9d8dc
Han-Wen Nienhuys <hanwen@google.com>
2015-09-30 11:23:51 +0200

    Provide more context in test failure messages.

--- fuse/nodefs/files_test.go
+++ fuse/nodefs/files_test.go
@@ -10,13 +10,13 @@ func TestTimeToTimeval(t *testing.T) {
 	date := time.Date(1960, time.January, 1, 23, 16, 44, 650951, time.UTC)
 	tv := timeToTimeval(&date)
 	if tv.Sec != -315535396 || tv.Usec != 650 {
-		t.Fail()
+		t.Errorf("got timeval %v, want (-315535396, 650)", tv)
 	}
 
 	// Check recent date
 	date = time.Date(2015, time.September, 29, 20, 8, 7, 74522, time.UTC)
 	tv = timeToTimeval(&date)
 	if tv.Sec != 1443557287 || tv.Usec != 74 {
-		t.Fail()
+		t.Errorf("got timeval %v, want (1443557287, 74)", tv)
 	}
 }
