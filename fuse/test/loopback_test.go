hanwen/go-fuse

commit 1b3f7953f928367b9e875c7969c0ceb2073bd8ad

nodefs: do not overwrite the "out.Ino" inode number
...if it has already been set by the filesystem.

"out.Ino" is the user-facing inode number that is displayed by "ls -li".

This change allows loopback filesystems to expose the inode number of
the underlying filesystem.
As a "real" inode number, this one is stable for the lifetime of the file,
even across remounts.

libfuse has the `use_ino` option that allows the inode numbers to be set
arbitrarily as well. This has been used by EncFS for years.

The Linux kernel stores the inode number in `orig_ino`. Grepping in
`fs/fuse` shows that it is only read once in `fuse_update_attributes`,
and only if the `stat` argument is not NULL. There is only one caller
that passes that argument: `fuse_getattr`.

In short, the kernel does not use the inode number for anything but
reporting it via `stat()`, hence setting the inode number is safe.

This functionality will be used for improving log output in gocryptfs,
where file names cannot be used because they are encrypted.

fuse/nodefs/fsconnector.go
@@ -89,7 +89,9 @@ func (c *rawBridge) childLookup(out *fuse.EntryOut, n *Inode, context *fuse.Cont
 	n.Node().GetAttr((*fuse.Attr)(&out.Attr), nil, context)
 	n.mount.fillEntry(out)
 	out.NodeId, out.Generation = c.fsConn().lookupUpdate(n)
-	out.Ino = out.NodeId
+	if out.Ino == 0 {
+		out.Ino = out.NodeId
+	}
 	if out.Nlink == 0 {
 		// With Nlink == 0, newer kernels will refuse link
 		// operations.

fuse/nodefs/fsmount.go
@@ -70,7 +70,9 @@ func (m *fileSystemMount) fillEntry(out *fuse.EntryOut) {
 func (m *fileSystemMount) fillAttr(out *fuse.AttrOut, nodeId uint64) {
 	splitDuration(m.options.AttrTimeout, &out.AttrValid, &out.AttrValidNsec)
 	m.setOwner(&out.Attr)
-	out.Ino = nodeId
+	if out.Ino == 0 {
+		out.Ino = nodeId
+	}
 }
 
 func (m *fileSystemMount) getOpenedFile(h uint64) *openedFile {

fuse/nodefs/fsops.go
@@ -99,7 +99,9 @@ func (c *rawBridge) Lookup(header *fuse.InHeader, name string, out *fuse.EntryOu
 
 	child.mount.fillEntry(out)
 	out.NodeId, out.Generation = c.fsConn().lookupUpdate(child)
-	out.Ino = out.NodeId
+	if out.Ino == 0 {
+		out.Ino = out.NodeId
+	}
 
 	return fuse.OK
 }

commit 2001c7c2071ebe6e31891c90492ed75da3bb770c

Fix TestLinkForget.
It was broken in 1b3f7953f9, which removed the possibility of
trivially inspecting go-fuse internal inode numbers.

fuse/test/loopback_test.go
@@ -345,20 +345,45 @@ func TestLinkForget(t *testing.T) {
 		t.Fatalf("Link failed: %v", err)
 	}
 
-	var s1, s2 syscall.Stat_t
-	err = syscall.Lstat(tc.mnt+"/file1", &s1)
-	if err != nil {
-		t.Fatalf("Lstat failed: %v", err)
+	for _, fn := range []string{"file1", "file2"} {
+		var s syscall.Stat_t
+		err = syscall.Lstat(tc.mnt+"/"+fn, &s)
+		if err != nil {
+			t.Fatalf("Lstat failed: %v", err)
+		}
+		tc.pathFs.ForgetClientInodes()
 	}
 
-	tc.pathFs.ForgetClientInodes()
-
-	err = syscall.Lstat(tc.mnt+"/file2", &s2)
-	if err != nil {
-		t.Fatalf("Lstat failed: %v", err)
+	// Now, the backing files are still hardlinked, but go-fuse's
+	// view of them should not be because of the
+	// ForgetClientInodes call. To prove this, we swap out the
+	// files in the backing store, and prove that they are
+	// distinct by truncating to different lengths.
+	for _, fn := range []string{"file1", "file2"} {
+		fn = tc.orig + "/" + fn
+		if err := os.Remove(fn); err != nil {
+			t.Fatalf("Remove", err)
+		}
+		if err := ioutil.WriteFile(fn, []byte(c), 0644); err != nil {
+			t.Fatalf("WriteFile", err)
+		}
 	}
-	if s1.Ino == s2.Ino {
-		t.Error("After forget, we should not export links")
+	for i, fn := range []string{"file1", "file2"} {
+		fn = tc.mnt + "/" + fn
+		if err := os.Truncate(fn, int64(i)); err != nil {
+			t.Fatalf("Truncate", err)
+		}
+	}
+
+	for i, fn := range []string{"file1", "file2"} {
+		var s syscall.Stat_t
+		err = syscall.Lstat(tc.mnt+"/"+fn, &s)
+		if err != nil {
+			t.Fatalf("Lstat failed: %v", err)
+		}
+		if s.Size != int64(i) {
+			t.Errorf("Lstat(%q): got size %d, want %d", fn, s.Size, i)
+		}
 	}
 }
 