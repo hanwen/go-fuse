534ef291c17df19a80e9f2f8ac7cbfa26f347287
Jakob Unterwurzacher <jakobunt@gmail.com>
2016-03-17 21:34:30 +0100

    fuse: add forgetMu mutex to prevent concurrent LOOKUP and FORGET
    Quoting from the patch:
    
     forgetMu prevents LOOKUP from handing out references to nodes that are
     in FORGET processing.
    
     Thread 1                  Thread 2
     --------                  --------
     FORGET X "foo"
     --> c.inodeMap.Forget()
                               LOOKUP "foo"
                               --> return NodeId Y
     --> node.OnForget()
    
     At this point the kernel would hold a reference to "node" that believes
     it has been forgotten.
    
    Change-Id: Ib32d3b250529dd65143b29d2f7a34fb8f9559af6

--- fuse/nodefs/fsconnector.go
+++ fuse/nodefs/fsconnector.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 	"unsafe"
 
@@ -31,6 +32,21 @@ type FileSystemConnector struct {
 
 	// The root of the FUSE file system.
 	rootNode *Inode
+
+	// forgetMu prevents LOOKUP from handing out references to nodes that are
+	// in FORGET processing.
+	//
+	// Thread 1                  Thread 2
+	// --------                  --------
+	// FORGET X "foo"
+	// --> c.inodeMap.Forget()
+	//                           LOOKUP "foo"
+	//                           --> return NodeId Y
+	// --> node.OnForget()
+	//
+	// At this point the kernel would hold a reference to "node" that believes
+	// it has been forgotten.
+	forgetMu sync.RWMutex
 }
 
 // NewOptions generates FUSE options that correspond to libfuse's
@@ -124,16 +140,18 @@ func (c *FileSystemConnector) forgetUpdate(nodeID uint64, forgetCount int) {
 		return
 	}
 
+	c.forgetMu.Lock()
 	if forgotten, handled := c.inodeMap.Forget(nodeID, forgetCount); forgotten {
 		inode := (*Inode)(unsafe.Pointer(handled))
-		node := inode.Node()
 		inode.mount.treeLock.RLock()
 		l := len(inode.children)
 		inode.mount.treeLock.RUnlock()
-		if l == 0 && node.Deletable() {
-			node.OnForget()
+		if l == 0 && inode.fsInode.Deletable() {
+			inode.fsInode.OnForget()
+			inode.fsInode = nil
 		}
 	}
+	c.forgetMu.Unlock()
 	// TODO - try to drop children even forget was not successful.
 	c.verify()
 }
