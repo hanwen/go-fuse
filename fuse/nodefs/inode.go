5e38b5b27b3251342b93a9a008d741a544474bf1
Han-Wen Nienhuys <hanwen@google.com>
2016-04-01 20:27:52 +0200

    fuse/nodefs: Add TreeWatcher interface.
    Use the TreeWatcher interface in pathfs to synchronize the parent
    mapping in pathfs with the tree management in nodefs.
    
    Tested:
      loopback -l $mount $orig
      fsstress -p 1 -z -f rmdir=10 -f link=10 -f creat=10 -f mkdir=10 \
        -f rename=30 -f stat=30 -f unlink=30 -f truncate=20 -m 8 -n 1000 \
        -d $mount -s 1456162204 ; sleep 1; echo ok ; rm -Rf $mount
    
    Fixes #99.
    
    Change-Id: I8668571c7dabf6da6ec708eec53de1887864b9ea

--- fuse/nodefs/inode.go
+++ fuse/nodefs/inode.go
@@ -142,6 +142,15 @@ func (n *Inode) AddChild(name string, child *Inode) {
 	n.mount.treeLock.Unlock()
 }
 
+// TreeWatcher is an additional interface that Nodes can implement.
+// If they do, the OnAdd and OnRemove are called for operations on the
+// file system tree. The functions run under a lock, so they should
+// not do blocking operations.
+type TreeWatcher interface {
+	OnAdd(parent *Inode, name string)
+	OnRemove(parent *Inode, name string)
+}
+
 // RmChild removes an inode by name, and returns it. It returns nil if
 // child does not exist.
 func (n *Inode) RmChild(name string) (ch *Inode) {
@@ -163,13 +172,19 @@ func (n *Inode) addChild(name string, child *Inode) {
 		}
 	}
 	n.children[name] = child
+	if w, ok := child.Node().(TreeWatcher); ok && child.mountPoint == nil {
+		w.OnAdd(n, name)
+	}
 }
 
 // Must be called with treeLock for the mount held.
-func (n *Inode) rmChild(name string) (ch *Inode) {
-	ch = n.children[name]
+func (n *Inode) rmChild(name string) *Inode {
+	ch := n.children[name]
 	if ch != nil {
 		delete(n.children, name)
+		if w, ok := ch.Node().(TreeWatcher); ok && ch.mountPoint == nil {
+			w.OnRemove(n, name)
+		}
 	}
 	return ch
 }
