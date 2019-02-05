2f61bee322526df565a8d32f7596b794e46b99dc
Han-Wen Nienhuys <hanwen@google.com>
2019-02-05 20:09:45 +0100

    nodefs: sketch for a better NodeFS API
    Co-author: navytux
    
    Thanks to rfjakob for general feedback.
    
    Next:
    - add refctr like libfuse. Use it to implement persistent nodes 
    
    TODO
    - context for PID/caller
    - Opendir
    
    Open questions: grep for NOSUBMIT/TODO
    - how many interfaces?
    - explode SetAttr ?

---
+++ nodefs/files_linux.go
@@ -0,0 +1,36 @@
+// Copyright 2019 the Go-FUSE Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package nodefs
+
+import (
+	"context"
+	"syscall"
+	"time"
+
+	"github.com/hanwen/go-fuse/fuse"
+)
+
+func (f *loopbackFile) Allocate(ctx context.Context, off uint64, sz uint64, mode uint32) fuse.Status {
+	f.mu.Lock()
+	err := syscall.Fallocate(int(f.File.Fd()), mode, int64(off), int64(sz))
+	f.mu.Unlock()
+	if err != nil {
+		return fuse.ToStatus(err)
+	}
+	return fuse.OK
+}
+
+// Utimens - file handle based version of loopbackFileSystem.Utimens()
+func (f *loopbackFile) Utimens(ctx context.Context, a *time.Time, m *time.Time) fuse.Status {
+	var ts [2]syscall.Timespec
+	ts[0] = fuse.UtimeToTimespec(a)
+	ts[1] = fuse.UtimeToTimespec(m)
+	f.mu.Lock()
+	var err error
+	// NOSUBMIT
+	//	err := futimens(int(f.File.Fd()), &ts)
+	f.mu.Unlock()
+	return fuse.ToStatus(err)
+}
---
+++ nodefs/loopback.go
@@ -0,0 +1,117 @@
+// Copyright 2019 the Go-FUSE Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package nodefs
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"syscall"
+
+	"github.com/hanwen/go-fuse/fuse"
+)
+
+type loopbackRoot struct {
+	loopbackNode
+
+	root string
+}
+
+func (n *loopbackRoot) GetAttr(ctx context.Context, f File, out *fuse.Attr) fuse.Status {
+	var err error = nil
+	st := syscall.Stat_t{}
+	err = syscall.Stat(n.root, &st)
+	if err != nil {
+		return fuse.ToStatus(err)
+	}
+	out.FromStat(&st)
+	return fuse.OK
+}
+
+type loopbackNode struct {
+	DefaultNode
+
+	rootNode *loopbackRoot
+}
+
+func (n *loopbackNode) path() string {
+	path := InodeOf(n).Path(nil)
+	return filepath.Join(n.rootNode.root, path)
+}
+
+func (n *loopbackNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*Inode, fuse.Status) {
+	p := filepath.Join(n.path(), name)
+
+	st := syscall.Stat_t{}
+	err := syscall.Lstat(p, &st)
+	if err != nil {
+		return nil, fuse.ToStatus(err)
+	}
+
+	out.Attr.FromStat(&st)
+
+	ch := InodeOf(n).FindChildByOpaqueID(name, out.Attr.Ino)
+	if ch != nil {
+		return ch, fuse.OK
+	}
+
+	node := &loopbackNode{rootNode: n.rootNode}
+	ch = n.inode().NewInode(node, out.Attr.Mode, out.Attr.Ino)
+	return ch, fuse.OK
+}
+
+func (n *loopbackNode) Create(ctx context.Context, name string, flags uint32, mode uint32) (inode *Inode, fh File, fuseFlags uint32, code fuse.Status) {
+	p := filepath.Join(n.path(), name)
+
+	f, err := os.OpenFile(p, int(flags)|os.O_CREATE, os.FileMode(mode))
+	if err != nil {
+		return nil, nil, 0, fuse.ToStatus(err)
+	}
+
+	st := syscall.Stat_t{}
+	if err := syscall.Fstat(int(f.Fd()), &st); err != nil {
+		f.Close()
+		return nil, nil, 0, fuse.ToStatus(err)
+	}
+
+	node := &loopbackNode{rootNode: n.rootNode}
+	ch := n.inode().NewInode(node, st.Mode, st.Ino)
+	return ch, NewLoopbackFile(f), 0, fuse.OK
+}
+
+func (n *loopbackNode) Open(ctx context.Context, flags uint32) (fh File, fuseFlags uint32, code fuse.Status) {
+	p := n.path()
+	f, err := os.OpenFile(p, int(flags), 0)
+	if err != nil {
+		return nil, 0, fuse.ToStatus(err)
+	}
+	return NewLoopbackFile(f), 0, fuse.OK
+}
+
+func (n *loopbackNode) GetAttr(ctx context.Context, f File, out *fuse.Attr) fuse.Status {
+	if f != nil {
+		return f.GetAttr(ctx, out)
+	}
+
+	p := n.path()
+
+	var err error = nil
+	st := syscall.Stat_t{}
+	err = syscall.Lstat(p, &st)
+	if err != nil {
+		return fuse.ToStatus(err)
+	}
+	out.FromStat(&st)
+	return fuse.OK
+}
+
+func NewLoopback(root string) Node {
+	n := &loopbackRoot{
+		root: root,
+	}
+	n.rootNode = n
+
+	return n
+}
