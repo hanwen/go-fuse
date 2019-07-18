4aed08d367dad87afa7c9beb65886f2d48f5d18c
Han-Wen Nienhuys <hanwen@google.com>
2019-07-18 09:55:13 +0200

    fs: add more package docs.
    Add dynamic_example_test.go to Readdir/Lookup.
    
    Change-Id: If7cd2a29166b9952af0ca362cba89df1d98b634b

--- fs/example_test.go
+++ fs/example_test.go
@@ -14,37 +14,30 @@ import (
 	"github.com/hanwen/go-fuse/fuse"
 )
 
-// mountLoopback mounts dir under the given mountpoint
-func mountLoopback(dir, mntPoint string) (*fuse.Server, error) {
-	root, err := fs.NewLoopbackRoot(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	// Make the root available under mntDir
-	return fs.Mount(mntPoint, root, &fs.Options{
-		MountOptions: fuse.MountOptions{Debug: true},
-	})
-}
-
 // An example of creating a loopback file system, and mounting it onto
 // a directory
 func Example_mountLoopback() {
 	mntDir, _ := ioutil.TempDir("", "")
-
 	home := os.Getenv("HOME")
-
 	// Make $HOME available on a mount dir under /tmp/ . Caution:
 	// write operations are also mirrored.
-	server, err := mountLoopback(mntDir, home)
+	root, err := fs.NewLoopbackRoot(home)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
+	}
+
+	// Mount the file system
+	server, err := fs.Mount(mntDir, root, &fs.Options{
+		MountOptions: fuse.MountOptions{Debug: true},
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Printf("Mounted %s as loopback on %s\n", home, mntDir)
 	fmt.Printf("\n\nCAUTION:\nwrite operations on %s will also affect $HOME (%s)\n\n", mntDir, home)
 	fmt.Printf("Unmount by calling 'fusermount -u %s'\n", mntDir)
 
-	// Wait until the directory is unmounted
+	// Serve the file system, until unmounted by calling fusermount -u
 	server.Wait()
 }
