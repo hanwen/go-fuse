975ad799d03d3d7f69ca8cacf3468c0dbb18519d
Han-Wen Nienhuys <hanwen@google.com>
2019-07-20 10:44:42 +0200

    fs: return OK for unimplemented Flush
    This simplifies writing R/O filesystems, which do not need flushing.
    
    Change-Id: I428627799904e30f486ff82bcafe2f9c4db88d01

--- fs/api.go
+++ fs/api.go
@@ -296,10 +296,10 @@ type NodeFsyncer interface {
 	Fsync(ctx context.Context, f FileHandle, flags uint32) syscall.Errno
 }
 
-// Flush is called for close() call on a file descriptor. In
-// case of duplicated descriptor, it may be called more than
-// once for a file.   The default implementation forwards to the
-// FileHandle.
+// Flush is called for close() call on a file descriptor. In case of
+// duplicated descriptor, it may be called more than once for a file.
+// The default implementation forwards to the FileHandle, or if
+// FileFlusher is not defined, returns OK.
 type NodeFlusher interface {
 	Flush(ctx context.Context, f FileHandle) syscall.Errno
 }
