33d5b625b1874d09f4b0c230e5ac20e72dd02086
Han-Wen Nienhuys <hanwen@google.com>
2019-07-20 13:29:17 +0200

    Update top-level documentation (README and doc.go)
    Change-Id: I451f56511a2b796c6a90df49e6c288d6675af363

---
+++ doc.go
@@ -0,0 +1,14 @@
+// Copyright 2019 the Go-FUSE Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This is a repository containing Go bindings for writing FUSE file
+// systems.
+//
+// Go to https://godoc.org/github.com/hanwen/go-fuse/fs for the
+// in-depth documentation for this library.
+//
+// Older, deprecated APIs are available at
+// https://godoc.org/github.com/hanwen/go-fuse/fuse/pathfs and
+// https://godoc.org/github.com/hanwen/go-fuse/fuse/nodefs.
+package lib
