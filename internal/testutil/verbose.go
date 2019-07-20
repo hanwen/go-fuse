85a3fb969717eaca08238ed3ab69e17f17aea5ec
Han-Wen Nienhuys <hanwen@google.com>
2019-07-20 12:58:24 +0200

    internal/testutil: switch off debug if test is called TestNonVerbose
    Change-Id: I71cd7cb713c7e864305ebad73a1b072395c13b27

--- internal/testutil/verbose.go
+++ internal/testutil/verbose.go
@@ -4,10 +4,20 @@
 
 package testutil
 
-import "flag"
+import (
+	"bytes"
+	"flag"
+	"runtime"
+)
 
 // VerboseTest returns true if the testing framework is run with -v.
 func VerboseTest() bool {
+	var buf [2048]byte
+	n := runtime.Stack(buf[:], false)
+	if bytes.Index(buf[:n], []byte("TestNonVerbose")) != -1 {
+		return false
+	}
+
 	flag := flag.Lookup("test.v")
 	return flag != nil && flag.Value.String() == "true"
 }
