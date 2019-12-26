e532b62f9b73a573755a44df92348c45b6664704
Jakob Unterwurzacher <jakobunt@gmail.com>
2019-12-26 16:45:56 +0100

    loopback: leave file permissions on "000" files as-is
    Set `opts.NullPermissions = true` to stop us from falsifying
    file permissions on "000" files.
    
    Fixes xfstests generic/088.
    
    Change-Id: Ibabbdc97b1ae2531ca093bae6bb441ae15d3238e

--- example/loopback/main.go
+++ example/loopback/main.go
@@ -104,6 +104,8 @@ func main() {
 	opts.MountOptions.Options = append(opts.MountOptions.Options, "fsname="+orig)
 	// Second column in "df -T" will be shown as "fuse." + Name
 	opts.MountOptions.Name = "loopback"
+	// Leave file permissions on "000" files as-is
+	opts.NullPermissions = true
 	server, err := fs.Mount(flag.Arg(0), loopbackRoot, opts)
 	if err != nil {
 		log.Fatalf("Mount fail: %v\n", err)
