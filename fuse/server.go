0589b29a198537829710005844799a7f172cf94a
Han-Wen Nienhuys <hanwen@google.com>
2023-06-09 18:35:00 +0200

    fuse: tweak Unmount doc comment
    Change-Id: I7e9b530906c233f7fcdac434984b9051acb7dbb9

--- fuse/server.go
+++ fuse/server.go
@@ -116,14 +116,14 @@ func (ms *Server) RecordLatencies(l LatencyMap) {
 
 // Unmount calls fusermount -u on the mount. This has the effect of
 // shutting down the filesystem. After the Server is unmounted, it
-// should be discarded.
+// should be discarded.  This function is idempotent.
 //
 // Does not work when we were mounted with the magic /dev/fd/N mountpoint syntax,
 // as we do not know the real mountpoint. Unmount using
 //
 //	fusermount -u /path/to/real/mountpoint
 //
-// / in this case.
+// in this case.
 func (ms *Server) Unmount() (err error) {
 	if ms.mountPoint == "" {
 		return nil
