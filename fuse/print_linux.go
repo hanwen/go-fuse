6d2f95e54447b2f6252b37ad1e470c643cb148cc
Kirill Smelkov <kirr@nexedi.com>
2023-06-14 14:05:23 +0300

    fuse: Print debug flags in stable order
    This helps to compare fuse logs of e.g. different test runs. For example
    below are two INIT messages with the same flags, but formatted, before
    hereby patch, differently:
    
         rx 2: INIT n0 {7.37 Ra 131072 EXPORT_SUPPORT,SPLICE_READ,AUTO_INVAL_DATA,ASYNC_DIO,WRITEBACK_CACHE,BIG_WRITES,NO_OPEN_SUPPORT,PARALLEL_DIROPS,POSIX_ACL,EXPLICIT_INVAL_DATA,NO_OPENDIR_SUPPORT,ASYNC_READ,DONT_MASK,SPLICE_MOVE,IOCTL_DIR,READDIRPLUS,READDIRPLUS_AUTO,HANDLE_KILLPRIV,POSIX_LOCKS,ATOMIC_O_TRUNC,SPLICE_WRITE,FLOCK_LOCKS,ABORT_ERROR,MAX_PAGES,CACHE_SYMLINKS,0x70000000} "\x03\x00\x00\x00\x00\x00\x00\x00"... 48b
    
         rx 2: INIT n0 {7.37 Ra 131072 EXPORT_SUPPORT,SPLICE_READ,AUTO_INVAL_DATA,ASYNC_DIO,WRITEBACK_CACHE,BIG_WRITES,NO_OPEN_SUPPORT,PARALLEL_DIROPS,POSIX_ACL,EXPLICIT_INVAL_DATA,ASYNC_READ,DONT_MASK,SPLICE_MOVE,IOCTL_DIR,READDIRPLUS,READDIRPLUS_AUTO,HANDLE_KILLPRIV,NO_OPENDIR_SUPPORT,POSIX_LOCKS,ATOMIC_O_TRUNC,SPLICE_WRITE,FLOCK_LOCKS,ABORT_ERROR,MAX_PAGES,CACHE_SYMLINKS,0x70000000} "\x03\x00\x00\x00\x00\x00\x00\x00"... 48b
    
    which leads to corresponding lines to be present in the diff and
    requires human attention to verify that the messages are really the
    same.
    
    After this patch above INIT always formats as
    
         rx 2: INIT n0 {7.37 Ra 131072 ASYNC_READ,POSIX_LOCKS,ATOMIC_O_TRUNC,EXPORT_SUPPORT,BIG_WRITES,DONT_MASK,SPLICE_WRITE,SPLICE_MOVE,SPLICE_READ,FLOCK_LOCKS,IOCTL_DIR,AUTO_INVAL_DATA,READDIRPLUS,READDIRPLUS_AUTO,ASYNC_DIO,WRITEBACK_CACHE,NO_OPEN_SUPPORT,PARALLEL_DIROPS,HANDLE_KILLPRIV,POSIX_ACL,ABORT_ERROR,MAX_PAGES,CACHE_SYMLINKS,NO_OPENDIR_SUPPORT,EXPLICIT_INVAL_DATA,0x70000000} "\x03\x00\x00\x00\x00\x00\x00\x00"... 48b
    
    and the diff log does not unnecessarily distract attention to it.
    
    flagString is used for many messages, this fix should benefit all of
    them.
    
    Change-Id: Id4bdab9dd90552313ede41a3b9a6c337588e4b3e

--- fuse/print_linux.go
+++ fuse/print_linux.go
@@ -10,9 +10,9 @@ import (
 )
 
 func init() {
-	openFlagNames[syscall.O_DIRECT] = "DIRECT"
-	openFlagNames[syscall.O_LARGEFILE] = "LARGEFILE"
-	openFlagNames[syscall_O_NOATIME] = "NOATIME"
+	openFlagNames.set(syscall.O_DIRECT, "DIRECT")
+	openFlagNames.set(syscall.O_LARGEFILE, "LARGEFILE")
+	openFlagNames.set(syscall_O_NOATIME, "NOATIME")
 }
 
 func (a *Attr) string() string {
