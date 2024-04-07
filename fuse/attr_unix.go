1072b7f041d80961ae136ca3fb58b6efdd050754
lch <lchopn@gmail.com>
2024-04-07 20:34:20 +0800

    fuse: add functional support for FreeBSD
    fuse: merge attr definition of Darwin and FreeBSD
    
    Change-Id: I7229229aed8afad74089bc2ce7eff0b86bd2ccd0

--- fuse/attr_unix.go
+++ fuse/attr_unix.go
@@ -1,4 +1,6 @@
-// Copyright 2016 the Go-FUSE Authors. All rights reserved.
+//go:build !linux
+
+// Copyright 2024 the Go-FUSE Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
--- fuse/server_unix.go
+++ fuse/server_unix.go
@@ -1,17 +1,15 @@
-// Copyright 2016 the Go-FUSE Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+//go:build !linux
 
 package fuse
 
 import (
-	"syscall"
+	"golang.org/x/sys/unix"
 )
 
 func (ms *Server) systemWrite(req *request, header []byte) Status {
 	if req.flatDataSize() == 0 {
 		err := handleEINTR(func() error {
-			_, err := syscall.Write(ms.mountFd, header)
+			_, err := unix.Write(ms.mountFd, header)
 			return err
 		})
 		return ToStatus(err)
