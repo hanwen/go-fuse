76685c7738e7b1207cc191a327cd1e20b1ba639a
Han-Wen Nienhuys <hanwen@google.com>
2024-08-15 13:39:37 +0200

    fuse: do not set 0x8000 for LARGEFILE on mips64.
    mips64 uses 0x4000 for LARGEFILE. See also issue #514	
    
    Change-Id: I46e83824f412131961318b16767bc05273fa2fbe

--- fuse/print_linux.go
+++ fuse/print_linux.go
@@ -5,13 +5,19 @@
 package fuse
 
 import (
+	"runtime"
+	"strings"
 	"syscall"
 )
 
 func init() {
-	// syscall.O_LARGEFILE is 0x0 on x86_64, but the
-	// kernel supplies 0x8000 anyway.
-	openFlagNames.set(0x8000, "LARGEFILE")
+	// syscall.O_LARGEFILE is 0x0 on x86_64, but the kernel
+	// supplies 0x8000 anyway, except on mips64el, where 0x8000 is
+	// used for O_DIRECT.
+	if !strings.Contains(runtime.GOARCH, "mips64") {
+		openFlagNames.set(0x8000, "LARGEFILE")
+	}
+
 	openFlagNames.set(syscall.O_DIRECT, "DIRECT")
 	openFlagNames.set(syscall_O_NOATIME, "NOATIME")
 	initFlagNames.set(CAP_NO_OPENDIR_SUPPORT, "NO_OPENDIR_SUPPORT")
