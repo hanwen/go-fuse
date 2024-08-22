69b9a424f5199b86b31ebb4a9f145436514ec164
Han-Wen Nienhuys <hanwenn@gmail.com>
2024-08-22 09:48:28 +0200

    fs: support passthrough mode
    If a returned file implements the FilePassthroughFder interface, we
    try to register the file in the kernel.
    
    Implement this for loopbackFile, and test the behavior.
    
    Benchmark (CPU pinned at 2Ghz):
    
    $ go build -v &&  go test -run "abc" -bench '(Libfuse|FD)' --passthrough_hp ~/vc/libfuse/build/example/passthrough_hp -test.cpu=1,2,4
    goos: linux
    goarch: amd64
    pkg: github.com/hanwen/go-fuse/v2/benchmark
    cpu: Intel(R) Core(TM) i5-8350U CPU @ 1.70GHz
    BenchmarkGoFuseFDRead     	    2054	    570915 ns/op	3673.31 MB/s	    2359 B/op	       6 allocs/op
    BenchmarkGoFuseFDRead-2   	    2029	    533035 ns/op	3934.36 MB/s	    3138 B/op	       5 allocs/op
    BenchmarkGoFuseFDRead-4   	    2500	    485836 ns/op	4316.58 MB/s	    4323 B/op	       5 allocs/op
    BenchmarkLibfuseHP        	    3040	    435188 ns/op	4818.95 MB/s	     128 B/op	       0 allocs/op
    BenchmarkLibfuseHP-2      	    2700	    448759 ns/op	4673.22 MB/s	     144 B/op	       0 allocs/op
    BenchmarkLibfuseHP-4      	    3463	    459536 ns/op	4563.63 MB/s	     112 B/op	       0 allocs/op
    PASS
    
    With passthrough enabled:
    
    $ go build -v &&  sudo go test -run "abc" -bench '(Libfuse|FD)'  --passthrough_hp ~/vc/libfuse/build/example/passthrough_hp -test.cpu=1,2,4
    goos: linux
    goarch: amd64
    pkg: github.com/hanwen/go-fuse/v2/benchmark
    cpu: Intel(R) Core(TM) i5-8350U CPU @ 1.70GHz
    BenchmarkGoFuseFDRead     	   11992	     98038 ns/op	21391.13 MB/s	      45 B/op	       0 allocs/op
    BenchmarkGoFuseFDRead-2   	   11982	     97382 ns/op	21535.21 MB/s	      45 B/op	       0 allocs/op
    BenchmarkGoFuseFDRead-4   	   12241	     97676 ns/op	21470.58 MB/s	      67 B/op	       0 allocs/op
    BenchmarkLibfuseHP        	   12020	     96968 ns/op	21627.24 MB/s	      18 B/op	       0 allocs/op
    BenchmarkLibfuseHP-2      	   11665	     98293 ns/op	21335.68 MB/s	      19 B/op	       0 allocs/op
    BenchmarkLibfuseHP-4      	   12318	     97313 ns/op	21550.49 MB/s	      18 B/op	       0 allocs/op
    PASS
    
    Change-Id: If8bde502a3450028f4d87ba61fa9c76ea3ea6c63

---
+++ fs/passthrough_test.go
@@ -0,0 +1,109 @@
+// Copyright 2024 the Go-FUSE Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package fs
+
+import (
+	"context"
+	"io"
+	"os"
+	"sync"
+	"syscall"
+	"testing"
+
+	"github.com/hanwen/go-fuse/v2/fuse"
+	"github.com/hanwen/go-fuse/v2/internal/testutil"
+)
+
+type rwRegisteringNode struct {
+	LoopbackNode
+
+	mu     sync.Mutex
+	reads  int
+	writes int
+}
+
+func (n *rwRegisteringNode) Read(ctx context.Context, f FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	n.reads++
+	return f.(FileReader).Read(ctx, dest, off)
+}
+
+func (n *rwRegisteringNode) Write(ctx context.Context, f FileHandle, data []byte, off int64) (written uint32, errno syscall.Errno) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	n.writes++
+	return f.(FileWriter).Write(ctx, data, off)
+}
+
+func TestPassthrough(t *testing.T) {
+	if os.Geteuid() != 0 {
+		t.Skip("passthrough requires CAP_SYS_ADMIN")
+	}
+
+	mnt := t.TempDir()
+	n := &rwRegisteringNode{}
+
+	rootData := &LoopbackRoot{
+		Path: t.TempDir(),
+		NewNode: func(rootData *LoopbackRoot, parent *Inode, name string, st *syscall.Stat_t) InodeEmbedder {
+			return n
+		},
+	}
+	n.RootData = rootData
+	root := &LoopbackNode{
+		RootData: rootData,
+	}
+	opts := &Options{}
+	opts.Debug = testutil.VerboseTest()
+	server, err := Mount(mnt, root, opts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Unmount()
+
+	fn := mnt + "/file"
+	want := "hello there"
+	if err := os.WriteFile(fn, []byte(want), 0666); err != nil {
+		t.Fatalf("WriteFile: %v", err)
+	}
+
+	f, err := os.Open(fn)
+	if err != nil {
+		t.Fatalf("Open: %v", err)
+	}
+	defer f.Close()
+
+	got, err := io.ReadAll(f)
+	if err != nil {
+		t.Fatalf("Open: %v", err)
+	}
+	if want != string(got) {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+	want2 := "xxxx"
+	if err := os.WriteFile(fn, []byte(want2), 0666); err != nil {
+		t.Fatalf("WriteFile: %v", err)
+	}
+
+	got2, err := os.ReadFile(fn)
+	if err != nil {
+		t.Fatalf("ReadFile: %v", err)
+	}
+	if string(got2) != want2 {
+		t.Errorf("got %q want %q", got2, want2)
+	}
+
+	f.Close()
+	server.Unmount()
+
+	if n.reads > 0 {
+		t.Errorf("got readcount %d want 0", n.reads)
+	}
+	if n.writes > 0 {
+		t.Errorf("got writecount %d want 0", n.writes)
+	}
+}
