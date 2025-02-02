hanwen/go-fuse

commit 334a5a5fdb971e514a874c5e4dce6b833ebd783d

fuse: update for kernel API version 40
Add definitions and print routines for FUSE_NO_EXPORT_SUPPORT,
FUSE_NOTIFY_RESEND and FOPEN_PASSTHROUGH and associated fields.

Change-Id: I5bd52c44a91484ac78ea5413cbd4bf5dc927501e

fuse/opcode.go
@@ -99,40 +99,41 @@ func doInit(server *Server, req *request) {
 		return
 	}
 
+	kernelFlags := uint64(input.Flags) | uint64(input.Flags2)<<32
 	server.reqMu.Lock()
 	server.kernelSettings = *input
-	server.kernelSettings.Flags = input.Flags & (CAP_ASYNC_READ | CAP_BIG_WRITES | CAP_FILE_OPS |
+	kernelFlags &= (CAP_ASYNC_READ | CAP_BIG_WRITES | CAP_FILE_OPS |
 		CAP_READDIRPLUS | CAP_NO_OPEN_SUPPORT | CAP_PARALLEL_DIROPS | CAP_MAX_PAGES | CAP_RENAME_SWAP)
 
 	if server.opts.EnableLocks {
-		server.kernelSettings.Flags |= CAP_FLOCK_LOCKS | CAP_POSIX_LOCKS
+		kernelFlags |= CAP_FLOCK_LOCKS | CAP_POSIX_LOCKS
 	}
 	if server.opts.EnableSymlinkCaching {
-		server.kernelSettings.Flags |= CAP_CACHE_SYMLINKS
+		kernelFlags |= CAP_CACHE_SYMLINKS
 	}
 	if server.opts.EnableAcl {
-		server.kernelSettings.Flags |= CAP_POSIX_ACL
+		kernelFlags |= CAP_POSIX_ACL
 	}
 	if server.opts.SyncRead {
 		// Clear CAP_ASYNC_READ
-		server.kernelSettings.Flags &= ^uint32(CAP_ASYNC_READ)
+		kernelFlags &= ^uint64(CAP_ASYNC_READ)
 	}
 	if server.opts.DisableReadDirPlus {
 		// Clear CAP_READDIRPLUS
-		server.kernelSettings.Flags &= ^uint32(CAP_READDIRPLUS)
+		kernelFlags &= ^uint64(CAP_READDIRPLUS)
 	}
 
-	dataCacheMode := input.Flags & CAP_AUTO_INVAL_DATA
+	dataCacheMode := kernelFlags & CAP_AUTO_INVAL_DATA
 	if server.opts.ExplicitDataCacheControl {
 		// we don't want CAP_AUTO_INVAL_DATA even if we cannot go into fully explicit mode
 		dataCacheMode = 0
 
-		explicit := input.Flags & CAP_EXPLICIT_INVAL_DATA
+		explicit := kernelFlags & CAP_EXPLICIT_INVAL_DATA
 		if explicit != 0 {
 			dataCacheMode = explicit
 		}
 	}
-	server.kernelSettings.Flags |= dataCacheMode
+	kernelFlags |= dataCacheMode
 
 	if input.Minor >= 13 {
 		server.setSplice()
@@ -149,13 +150,12 @@ func doInit(server *Server, req *request) {
 		Major:               _FUSE_KERNEL_VERSION,
 		Minor:               _OUR_MINOR_VERSION,
 		MaxReadAhead:        input.MaxReadAhead,
-		Flags:               server.kernelSettings.Flags,
 		MaxWrite:            uint32(server.opts.MaxWrite),
 		CongestionThreshold: uint16(server.opts.MaxBackground * 3 / 4),
 		MaxBackground:       uint16(server.opts.MaxBackground),
 		MaxPages:            uint16(maxPages),
 	}
-
+	out.setFlags(kernelFlags)
 	if server.opts.MaxReadAhead != 0 && uint32(server.opts.MaxReadAhead) < out.MaxReadAhead {
 		out.MaxReadAhead = uint32(server.opts.MaxReadAhead)
 	}

fuse/types.go
@@ -256,12 +256,13 @@ const (
 	FOPEN_STREAM                 = (1 << 4)
 	FOPEN_NOFLUSH                = (1 << 5)
 	FOPEN_PARALLEL_DIRECT_WRITES = (1 << 6)
+	FOPEN_PASSTHROUGH            = (1 << 7)
 )
 
 type OpenOut struct {
 	Fh        uint64
 	OpenFlags uint32
-	Padding   uint32
+	BackingID int32
 }
 
 // To be set in InitIn/InitOut.Flags.
@@ -303,7 +304,10 @@ const (
 	CAP_HAS_INODE_DAX     = (1 << 33)
 	CAP_CREATE_SUPP_GROUP = (1 << 34)
 	CAP_HAS_EXPIRE_ONLY   = (1 << 35)
-	CAP_DIRECT_IO_RELAX   = (1 << 36)
+	CAP_DIRECT_IO_RELAX   = (1 << 36) // DIRECT_IO_ALLOW_MMAP ?
+	CAP_PASSTHROUGH       = (1 << 37)
+	CAP_NO_EXPORT_SUPPORT = (1 << 38)
+	CAP_HAS_RESEND        = (1 << 39)
 )
 
 type InitIn struct {
@@ -329,7 +333,13 @@ type InitOut struct {
 	MaxPages            uint16
 	Padding             uint16
 	Flags2              uint32
-	Unused              [7]uint32
+	MaxStackDepth       uint32
+	Unused              [6]uint32
+}
+
+func (o *InitOut) setFlags(flags uint64) {
+	o.Flags = uint32(flags)
+	o.Flags2 = uint32(flags >> 32)
 }
 
 type _CuseInitIn struct {
@@ -516,6 +526,7 @@ const (
 	NOTIFY_STORE_CACHE    = -4 // store data into kernel cache of an inode
 	NOTIFY_RETRIEVE_CACHE = -5 // retrieve data from kernel cache of an inode
 	NOTIFY_DELETE         = -6 // notify kernel that a directory entry has been deleted
+	NOTIFY_RESEND         = -7
 
 // NOTIFY_CODE_MAX     = -6
 )

fuse/print.go
@@ -50,6 +50,9 @@ var (
 		{CAP_CREATE_SUPP_GROUP, "CREATE_SUPP_GROUP"},
 		{CAP_HAS_EXPIRE_ONLY, "HAS_EXPIRE_ONLY"},
 		{CAP_DIRECT_IO_RELAX, "DIRECT_IO_RELAX"},
+		{CAP_PASSTHROUGH, "PASSTHROUGH"},
+		{CAP_NO_EXPORT_SUPPORT, "NO_EXPORT_SUPPORT"},
+		{CAP_HAS_RESEND, "HAS_RESEND"},
 	})
 	releaseFlagNames = newFlagNames([]flagNameEntry{
 		{RELEASE_FLUSH, "FLUSH"},
@@ -74,6 +77,9 @@ var (
 		{FOPEN_NONSEEKABLE, "NONSEEK"},
 		{FOPEN_CACHE_DIR, "CACHE_DIR"},
 		{FOPEN_STREAM, "STREAM"},
+		{FOPEN_NOFLUSH, "NOFLUSH"},
+		{FOPEN_PARALLEL_DIRECT_WRITES, "PARALLEL_DIRECT_WRITES"},
+		{FOPEN_PASSTHROUGH, "PASSTHROUGH"},
 	})
 	accessFlagName = newFlagNames([]flagNameEntry{
 		{X_OK, "x"},
@@ -205,7 +211,11 @@ func (in *OpenIn) string() string {
 }
 
 func (in *OpenOut) string() string {
-	return fmt.Sprintf("{Fh %d %s}", in.Fh,
+	backing := ""
+	if in.BackingID != 0 {
+		backing = fmt.Sprintf("backing=%d ", in.BackingID)
+	}
+	return fmt.Sprintf("{Fh %d %s %s}", in.Fh, backing,
 		flagString(fuseOpenFlagNames, int64(in.OpenFlags), ""))
 }
 
@@ -216,11 +226,11 @@ func (in *InitIn) string() string {
 }
 
 func (o *InitOut) string() string {
-	return fmt.Sprintf("{%d.%d Ra %d %s %d/%d Wr %d Tg %d MaxPages %d}",
+	return fmt.Sprintf("{%d.%d Ra %d %s %d/%d Wr %d Tg %d MaxPages %d MaxStack %d}",
 		o.Major, o.Minor, o.MaxReadAhead,
-		flagString(initFlagNames, int64(o.Flags), ""),
+		flagString(initFlagNames, int64(o.Flags)|int64(o.Flags2)<<32, ""),
 		o.CongestionThreshold, o.MaxBackground, o.MaxWrite,
-		o.TimeGran, o.MaxPages)
+		o.TimeGran, o.MaxPages, o.MaxStackDepth)
 }
 
 func (s *FsyncIn) string() string {

fuse/types_linux.go
@@ -23,12 +23,13 @@ const (
 const (
 	CAP_NO_OPENDIR_SUPPORT  = (1 << 24)
 	CAP_EXPLICIT_INVAL_DATA = (1 << 25)
-	CAP_MAP_ALIGNMENT       = (1 << 26)
-	CAP_SUBMOUNTS           = (1 << 27)
-	CAP_HANDLE_KILLPRIV_V2  = (1 << 28)
-	CAP_SETXATTR_EXT        = (1 << 29)
-	CAP_INIT_EXT            = (1 << 30)
-	CAP_INIT_RESERVED       = (1 << 31)
+
+	CAP_MAP_ALIGNMENT      = (1 << 26)
+	CAP_SUBMOUNTS          = (1 << 27)
+	CAP_HANDLE_KILLPRIV_V2 = (1 << 28)
+	CAP_SETXATTR_EXT       = (1 << 29)
+	CAP_INIT_EXT           = (1 << 30)
+	CAP_INIT_RESERVED      = (1 << 31)
 
 	// CAP_RENAME_SWAP only exists on OSX.
 	CAP_RENAME_SWAP = 0x0

commit 9fbd4ce3b31133cffef4d34ce5aefff7342713da

fuse: fix CAP_AUTO_INVAL_DATA handling in Init()
This was broken in commit 334a5a5 ("fuse: update for kernel API
version 40", Aug 23, 2024).

Add a test. Thanks to https://github.com/lllxin037 for diagnosing the
problem.

Closes https://github.com/hanwen/go-fuse/issues/559

Change-Id: I5e4fd55613dd7b810d7ce755cc754b50da71a6d2

fuse/opcode.go
@@ -124,17 +124,12 @@ func doInit(server *protocolServer, req *request) {
 		kernelFlags &= ^uint64(CAP_ALLOW_IDMAP)
 	}
 
-	dataCacheMode := kernelFlags & CAP_AUTO_INVAL_DATA
 	if server.opts.ExplicitDataCacheControl {
 		// we don't want CAP_AUTO_INVAL_DATA even if we cannot go into fully explicit mode
-		dataCacheMode = 0
-
-		explicit := kernelFlags & CAP_EXPLICIT_INVAL_DATA
-		if explicit != 0 {
-			dataCacheMode = explicit
-		}
+		kernelFlags |= input.Flags64() & CAP_EXPLICIT_INVAL_DATA
+	} else {
+		kernelFlags |= input.Flags64() & CAP_AUTO_INVAL_DATA
 	}
-	kernelFlags |= dataCacheMode
 
 	// maxPages is the maximum request size we want the kernel to use, in units of
 	// memory pages (usually 4kiB). Linux v4.19 and older ignore this and always use

fuse/types.go
@@ -286,7 +286,7 @@ const (
 	CAP_SPLICE_READ      = (1 << 9)
 	CAP_FLOCK_LOCKS      = (1 << 10)
 	CAP_IOCTL_DIR        = (1 << 11)
-	CAP_AUTO_INVAL_DATA  = (1 << 12)
+	CAP_AUTO_INVAL_DATA  = (1 << 12) // mtime changes invalidate page cache.
 	CAP_READDIRPLUS      = (1 << 13)
 	CAP_READDIRPLUS_AUTO = (1 << 14)
 	CAP_ASYNC_DIO        = (1 << 15)

fs/cache_test.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/hanwen/go-fuse/v2/fuse"
 	"github.com/hanwen/go-fuse/v2/internal/testutil"
+	"golang.org/x/sys/unix"
 )
 
 type keepCacheFile struct {
@@ -235,5 +236,86 @@ func TestSymlinkCaching(t *testing.T) {
 	}
 	if c := link.count(); c != 2 {
 		t.Errorf("got %d want 2", c)
+	}
+}
+
+type mmapTestNode struct {
+	Inode
+
+	mu      sync.Mutex
+	content []byte
+	mtime   time.Time
+}
+
+var _ = (NodeOpener)((*mmapTestNode)(nil))
+
+func (f *mmapTestNode) Open(ctx context.Context, openFlags uint32) (fh FileHandle, fuseFlags uint32, errno syscall.Errno) {
+	return nil, 0, 0
+}
+
+var _ = (NodeReader)((*mmapTestNode)(nil))
+
+func (f *mmapTestNode) Read(ctx context.Context, fh FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	return fuse.ReadResultData(f.content[off : int(off)+len(dest)]), OK
+}
+
+var _ = (NodeGetattrer)((*mmapTestNode)(nil))
+
+func (f *mmapTestNode) Getattr(ctx context.Context, fh FileHandle, out *fuse.AttrOut) syscall.Errno {
+	out.Size = uint64(len(f.content))
+	out.SetTimes(nil, &f.mtime, nil)
+	return OK
+}
+
+/* Exercise mmap() and AUTO_INVAL_DATA */
+func TestMMap(t *testing.T) {
+	mnt := t.TempDir()
+
+	node := mmapTestNode{
+		content: bytes.Repeat([]byte{'x'}, 4096),
+		mtime:   time.Now(),
+	}
+	root := &Inode{}
+	dt := 10 * time.Millisecond
+	opts := &Options{
+		EntryTimeout: &dt,
+		AttrTimeout:  &dt,
+		OnAdd: func(ctx context.Context) {
+			root.AddChild("file",
+				root.NewPersistentInode(ctx, &node, StableAttr{Mode: syscall.S_IFREG}), false)
+		},
+	}
+	opts.Debug = testutil.VerboseTest()
+
+	srv, err := Mount(mnt, root, opts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer srv.Unmount()
+	f, err := os.Open(mnt + "/file")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	data, err := unix.Mmap(int(f.Fd()), 0, int(8<<10), unix.PROT_READ, unix.MAP_SHARED)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer unix.Munmap(data)
+	if data[1] != 'x' {
+		t.Errorf("got %c want x", data[1])
+	}
+
+	node.mu.Lock()
+	node.mtime = node.mtime.Add(time.Hour)
+	node.content = bytes.Repeat([]byte{'y'}, 4096)
+	node.mu.Unlock()
+
+	if _, err := f.Stat(); err != nil {
+		t.Fatal(err)
+	}
+
+	if data[1] != 'y' {
+		t.Errorf("got %c want y", data[1])
 	}
 }