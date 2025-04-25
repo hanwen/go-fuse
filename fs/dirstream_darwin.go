9918d698917e8cf7db389a652683aee066d6d99b
Han-Wen Nienhuys <hanwenn@gmail.com>
2025-04-25 19:24:37 +0200

    fuse: remove trailing \0 more efficiently.
    Change-Id: Id00000005679dc05df2aab183b166cf123e3d2f1

--- fs/dirstream_darwin.go
+++ fs/dirstream_darwin.go
@@ -1,11 +1,11 @@
-//go:build !darwin
-
 // Copyright 2019 the Go-FUSE Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package fs
 
-func getdents(fd uintptr, buf []byte) (int, error) {
+import "golang.org/x/sys/unix"
+
+func getdents(fd int, buf []byte) (int, error) {
 	return unix.Getdirentries(fd, buf, nil)
 }
