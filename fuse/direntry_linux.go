73057da4334f2c349ed60926769b3aa30f14914d
Han-Wen Nienhuys <hanwenn@gmail.com>
2025-04-25 19:24:37 +0200

    fuse: add direntry.nameLength() and use it for parsing
    This is more likely to be correct on Darwin/FreeBSD.
    
    Change-Id: Id0000000025ed0dc17034a63b38ff5effa4b8402

--- fuse/direntry_linux.go
+++ fuse/direntry_linux.go
@@ -4,6 +4,8 @@
 
 package fuse
 
+import "unsafe"
+
 // Like syscall.Dirent, but without the [256]byte name.
 type dirent struct {
 	Ino    uint64
@@ -11,4 +13,8 @@ type dirent struct {
 	Reclen uint16
 	Type   uint8
 	Name   [1]uint8 // align to 4 bytes for 32 bits.
+}
+
+func (de *dirent) nameLength() int {
+	return int(de.Reclen) - int(unsafe.Offsetof(dirent{}.Name))
 }
