bf8d82c869fd0c311f8e944fa3f90f02098197ac
Han-Wen Nienhuys <hanwenn@gmail.com>
2025-04-25 19:24:37 +0200

    fs: on Darwin, read direntries directly from the file descriptor.
    For some reason, the x/sys/ package does not provide unix.Getdents on
    Darwin, but does provide unix.Getdirentries(), which is a BSD (?)
    flavor of the same? It takes a basep argument which is ignored.
    
    https://github.com/hanwen/go-fuse/issues/566
    
    Change-Id: Id000000049819d114496fca39cb947fb8bd5c210

--- fs/dirstream_darwin.go
+++ fs/dirstream_darwin.go
@@ -1,45 +1,11 @@
+//go:build !darwin
+
 // Copyright 2019 the Go-FUSE Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package fs
 
-import (
-	"io"
-	"os"
-	"syscall"
-
-	"github.com/hanwen/go-fuse/v2/fuse"
-)
-
-func NewLoopbackDirStreamFd(fd int) (DirStream, syscall.Errno) {
-	f := os.NewFile(uintptr(fd), "")
-	defer f.Close()
-
-	var entries []fuse.DirEntry
-	for {
-		want := 100
-		infos, err := f.Readdir(want)
-		for _, info := range infos {
-			s := fuse.ToStatT(info)
-			if s == nil {
-				continue
-			}
-
-			entries = append(entries, fuse.DirEntry{
-				Name: info.Name(),
-				Mode: uint32(s.Mode),
-				Ino:  s.Ino,
-			})
-		}
-		if len(infos) < want || err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			return nil, ToErrno(err)
-		}
-	}
-
-	return &dirArray{entries: entries}, OK
+func getdents(fd uintptr, buf []byte) (int, error) {
+	return unix.Getdirentries(fd, buf, nil)
 }
