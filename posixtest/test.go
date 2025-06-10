79eacd78bd59b3529eed02f0a151564133d7fef0
Han-Wen Nienhuys <hanwenn@gmail.com>
2025-06-10 09:12:22 +0200

    posixtest: add test for FALLOC_FL_KEEP_SIZE
    Change-Id: Id0000000621f276de323f0b91d33c76034427944

--- posixtest/test.go
+++ posixtest/test.go
@@ -9,6 +9,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -47,6 +48,7 @@ var All = map[string]func(*testing.T, string){
 	"DirectIO":                   DirectIO,
 	"OpenAt":                     OpenAt,
 	"Fallocate":                  Fallocate,
+	"FallocateKeepSize":          FallocateKeepSize,
 	"DirSeek":                    DirSeek,
 	"FcntlFlockSetLk":            FcntlFlockSetLk,
 	"FcntlFlockLocksFile":        FcntlFlockLocksFile,
@@ -688,6 +690,31 @@ func Fallocate(t *testing.T, mnt string) {
 	if fi.Size() < (1024 + 4096) {
 		t.Fatalf("fallocate should have changed file size. Got %d bytes",
 			fi.Size())
+	}
+}
+
+func FallocateKeepSize(t *testing.T, mnt string) {
+	f, err := os.Create(mnt + "/file")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	data := bytes.Repeat([]byte{42}, 100)
+	if _, err := f.Write(data); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := syscall.Fallocate(int(f.Fd()), unix.FALLOC_FL_KEEP_SIZE, 50, 52); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := f.Seek(0, io.SeekStart); err != nil {
+		t.Fatal(err)
+	}
+
+	roundtrip, _ := io.ReadAll(f)
+	if !bytes.Equal(roundtrip, data) {
+		t.Fatalf("roundtrip not equal %q != %q", roundtrip, data)
 	}
 }
 
